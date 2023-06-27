0bf594bc664a927f0495777080db5f966b847da4
Unknown-Pplayer <xwang2230@gmail.com>
2023-06-27 14:04:37 -0400

    clean up

--- index.js
+++ index.js
@@ -23,6 +23,39 @@ app.get("/student", async (req, res) => {
   }
 });
 
+app.get("/instructor", async (req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT id, name, department, major, course_id FROM instructor`
+    );
+    res.send(result.rows);
+  } catch (error) {
+    console.log("Error is caught:", error);
+  }
+});
+
+app.get("/course", async (req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT id, name, credit FROM course`
+    );
+    res.send(result.rows);
+  } catch (error) {
+    console.log("Error is caught:", error);
+  }
+});
+
+app.get("/department", async (req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT id, name, course_id, instructor_id FROM department`
+    );
+    res.send(result.rows);
+  } catch (error) {
+    console.log("Error is caught:", error);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
